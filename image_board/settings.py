43164f47b10e7a71f4de45a66f430e881e3b697b
neoToad <colinjmass@gmail.com>
2021-07-27 15:06:37 -0400

    heroku

--- image_board/settings.py
+++ image_board/settings.py
@@ -162,7 +162,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Heroku Settings
 import django_heroku
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
