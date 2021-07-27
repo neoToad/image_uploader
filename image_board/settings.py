9ecc7f8b3c3b4f3702bbe83ff21464b1a704e3f5
neoToad <colinjmass@gmail.com>
2021-07-27 10:26:48 -0400

    set Debug

--- image_board/settings.py
+++ image_board/settings.py
@@ -150,3 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
 
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
