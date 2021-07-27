0ef34f521bbeda2078a773167bf8c360ef0aef4a
neoToad <colinjmass@gmail.com>
2021-07-27 14:40:33 -0400

    heroku staging

--- image_board/settings.py
+++ image_board/settings.py
@@ -159,13 +159,13 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # Heroku Settings
-# import django_heroku
-# #
-# django_heroku.settings(locals())
-#
-# if os.environ.get('DEBUG') == 'TRUE':
-#     DEBUG = True
-# elif os.environ.get('DEBUG') == 'FALSE':
-#     DEBUG = False
+# Heroku Settings
+import django_heroku
+
+django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 #
